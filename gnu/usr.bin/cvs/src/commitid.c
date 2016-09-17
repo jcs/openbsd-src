f6df065956bbbd8d19fd7abea7d8982346f58c4a
joshua stein <jcs@jcs.org>
2016-09-17 17:53:28 -0500

    cvs: use SHA512_256, separate commitid pieces with :, hex version

--- gnu/usr.bin/cvs/src/commitid.c
+++ gnu/usr.bin/cvs/src/commitid.c
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016 joshua stein <jcs@openbsd.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #include "cvs.h"
 
 #include <sys/types.h>
@@ -70,14 +86,14 @@ commitid_parse(char *repo, char *id)
 	unsigned int version;
 	char hash[COMMITID_HASH_LENGTH];
 	unsigned long changeset = 0;
-	char fmt[20];
+	char fmt[22];
 	int res;
 
 	if (!strlen(id))
 		return NULL;
 
-	/* %04u%64s%07lu */
-	snprintf(fmt, sizeof(fmt), "%%0%du%%%ds%%0%dlu",
+	/* %04u:%64s:%07lu */
+	snprintf(fmt, sizeof(fmt), "%%0%du:%%%ds:%%0%dlu",
 	    COMMITID_VERSION_LENGTH, COMMITID_HASH_LENGTH,
 	    COMMITID_CHANGESET_LENGTH);
 
@@ -328,7 +344,7 @@ commitid_gen_start(char *repo, unsigned long changeset)
 	out->files = getlist();
 	out->genesis = (changeset == 0);
 
-	SHA256Init(&out->sha_ctx);
+	SHA512_256Init(&out->sha_ctx);
 
 	return out;
 }
@@ -359,7 +375,7 @@ commitid_gen_add_show(CommitId *id)
 int
 commitid_gen_add_buf(CommitId *id, uint8_t *buf, size_t len)
 {
-	SHA256Update(&id->sha_ctx, buf, len);
+	SHA512_256Update(&id->sha_ctx, buf, len);
 
 	return 1;
 }
@@ -408,10 +424,10 @@ int
 commitid_gen_final(CommitId *id)
 {
 	uint8_t *thash = xmalloc(COMMITID_HASH_LENGTH + 1);
-	char fmt[20];
+	char fmt[22];
 	int i;
 
-	SHA256Final(thash, &id->sha_ctx);
+	SHA512_256Final(thash, &id->sha_ctx);
 
 	/* digest to hex */
 	id->hash[0] = '\0';
@@ -421,8 +437,8 @@ commitid_gen_final(CommitId *id)
 
 	free(thash);
 
-	/* %04u%64s%07lu */
-	snprintf(fmt, sizeof(fmt), "%%0%du%%%ds%%0%dlu",
+	/* %04u:%64s:%07lu */
+	snprintf(fmt, sizeof(fmt), "%%0%du:%%%ds:%%0%dlu",
 	    COMMITID_VERSION_LENGTH, COMMITID_HASH_LENGTH,
 	    COMMITID_CHANGESET_LENGTH);
 
