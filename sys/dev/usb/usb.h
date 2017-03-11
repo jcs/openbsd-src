729e4a6a302fd5c5a87b4921c4ddfdf93cb61686
joshua stein <jcs@jcs.org>
2017-03-11 12:24:27 -0600

    usb: use spec timeouts
    saves about 3 seconds during boot

--- sys/dev/usb/usb.h
+++ sys/dev/usb/usb.h
@@ -601,7 +601,7 @@ typedef struct usb_port_status usb_port_status_t;
 #define USB_POWER_DOWN_TIME	200 /* ms */
 #define USB_PORT_POWER_DOWN_TIME	100 /* ms */
 
-#if 0
+#if 1
 /* These are the values from the spec. */
 #define USB_PORT_RESET_DELAY	10  /* ms */
 #define USB_PORT_ROOT_RESET_DELAY 50  /* ms */
