8889105488bc92796b72500384210f53c1ffb2ba
joshua stein <jcs@jcs.org>
2017-07-21 23:14:41 -0500

    sptgpio: more progress

--- sys/dev/acpi/sptgpio.c
+++ sys/dev/acpi/sptgpio.c
@@ -27,17 +27,40 @@
 #include <dev/acpi/amltypes.h>
 #include <dev/acpi/dsdt.h>
 
-#define SPTGPIO_CONF_GD_LEVEL	0x01000000
-#define SPTGPIO_CONF_GD_TPE	0x02000000
-#define SPTGPIO_CONF_GD_TNE	0x04000000
-#define SPTGPIO_CONF_GD_MASK	0x07000000
-#define SPTGPIO_CONF_DIRECT_IRQ_EN	0x08000000
+#define GENMASK(l, h)		((~0U >> (32 - h -1)) & (~0U << l))
 
-#define SPTGPIO_PAD_VAL		0x00000001
+#define SPT_REVID		0x0
+#define SPT_REVID_SHIFT		16
+#define SPT_REVID_MASK		GENMASK(31, 16)
 
-#define SPTGPIO_IRQ_TS_0	0x800
-#define SPTGPIO_IRQ_TS_1	0x804
-#define SPTGPIO_IRQ_TS_2	0x808
+#define SPT_PADBAR		0x00c
+
+#define SPTLP_IE_OFFSET		0x120
+#define SPTLP_GPP_SIZE		24
+
+struct spt_community {
+	int barno;
+	int first_pin;
+	int last_pin;
+	int gpp_size;
+	int ie_offset;
+
+	bus_space_handle_t sc_memh;
+	bus_addr_t sc_addr;
+	bus_size_t sc_size;
+
+	int features;
+
+	uint32_t padbar;
+};
+
+/* Sunrisepoint-LP */
+const struct spt_community sptlp_communities[] = {
+	{ 0,	0,	47,	SPTLP_GPP_SIZE,	SPTLP_IE_OFFSET },
+	{ 1,	48,	119,	SPTLP_GPP_SIZE,	SPTLP_IE_OFFSET },
+	{ 2,	120,	151,	SPTLP_GPP_SIZE,	SPTLP_IE_OFFSET },
+};
+#define SPTLP_NCOMMUNITIES 3
 
 struct sptgpio_intrhand {
 	int (*ih_func)(void *);
@@ -50,9 +73,8 @@ struct sptgpio_softc {
 	struct aml_node *sc_node;
 
 	bus_space_tag_t sc_memt;
-	bus_space_handle_t sc_memh;
-	bus_addr_t sc_addr;
-	bus_size_t sc_size;
+	struct spt_community *sc_comms;
+	int sc_ncomms;
 
 	int sc_irq;
 	int sc_irq_flags;
@@ -81,14 +103,6 @@ const char *sptgpio_hids[] = {
 	NULL
 };
 
-/*
- * The pads for the pins are randomly ordered.
- */
-
-const int sptlp_pins[] = {
-	64, 65,
-};
-
 int	sptgpio_parse_resources(int, union acpi_resource *, void *);
 int	sptgpio_read_pin(void *, int);
 void	sptgpio_write_pin(void *, int, int);
@@ -104,25 +118,64 @@ sptgpio_match(struct device *parent, void *match, void *aux)
 	return acpi_matchhids(aaa, sptgpio_hids, cf->cf_driver->cd_name);
 }
 
+int
+sptgpio_pin_community(struct sptgpio_softc *sc, int pin)
+{
+	int i;
+
+	for (i = 0; i < sc->sc_ncomms; i++) {
+		if (pin >= sc->sc_comms[i].first_pin &&
+		    pin <= sc->sc_comms[i].last_pin)
+			return i;
+	}
+
+	return -1;
+}
+
+void
+sptgpio_irq_mask(struct sptgpio_softc *sc, int pin, int mask)
+{
+	int gpp_offset, gpp, pad;
+	struct spt_community *c;
+	uint32_t v;
+
+	int comm = sptgpio_pin_community(sc, pin);
+	if (comm == -1)
+		return;
+
+	c = &sc->sc_comms[comm];
+	pad = (pin - c->first_pin);
+
+	gpp_offset = pad % c->gpp_size;
+	gpp = pad / c->gpp_size;
+
+	v = bus_space_read_4(sc->sc_memt, c->sc_memh, c->ie_offset + (gpp * 4));
+	if (mask)
+		v &= ~(1UL << gpp_offset);
+	else
+		v |= (1UL << gpp_offset);
+	bus_space_write_4(sc->sc_memt, c->sc_memh, c->ie_offset + (gpp * 4), v);
+}
+
 void
 sptgpio_attach(struct device *parent, struct device *self, void *aux)
 {
 	struct acpi_attach_args *aaa = aux;
 	struct sptgpio_softc *sc = (struct sptgpio_softc *)self;
 	struct aml_value res;
-	struct aml_value arg[2];
-	struct aml_node *node;
-	uint32_t reg;
-	int i;
-
-	return;
+	int i, j;
 
 	sc->sc_acpi = (struct acpi_softc *)parent;
 	sc->sc_node = aaa->aaa_node;
 	printf(": %s", sc->sc_node->name);
 
-	sc->sc_pins = sptlp_pins;
-	sc->sc_npins = nitems(sptlp_pins);
+	/* TODO: determine sptlp/spt-h */
+	if (1) {
+		sc->sc_comms = malloc(sizeof(sptlp_communities), M_DEVBUF,
+		    M_NOWAIT | M_ZERO);
+		memcpy(sc->sc_comms, sptlp_communities,
+		    sizeof(sptlp_communities));
+	}
 
 	if (aml_evalname(sc->sc_acpi, sc->sc_node, "_CRS", 0, NULL, &res)) {
 		printf(", can't find registers\n");
@@ -130,12 +183,14 @@ sptgpio_attach(struct device *parent, struct device *self, void *aux)
 	}
 
 	aml_parse_resource(&res, sptgpio_parse_resources, sc);
-	printf(" addr 0x%lx/0x%lx", sc->sc_addr, sc->sc_size);
-	if (sc->sc_addr == 0 || sc->sc_size == 0) {
-		printf("\n");
+	aml_freevalue(&res);
+
+	if (!sc->sc_ncomms) {
+		printf(", no communities\n");
 		return;
 	}
-	aml_freevalue(&res);
+
+	sc->sc_npins = sc->sc_comms[sc->sc_ncomms - 1].last_pin + 1;
 
 	sc->sc_pin_ih = mallocarray(sc->sc_npins, sizeof(*sc->sc_pin_ih),
 	    M_DEVBUF, M_NOWAIT | M_ZERO);
@@ -147,10 +202,16 @@ sptgpio_attach(struct device *parent, struct device *self, void *aux)
 	printf(" irq %d", sc->sc_irq);
 
 	sc->sc_memt = aaa->aaa_memt;
-	if (bus_space_map(sc->sc_memt, sc->sc_addr, sc->sc_size, 0,
-	    &sc->sc_memh)) {
-		printf(", can't map registers\n");
-		goto free;
+	for (i = 0; i < sc->sc_ncomms; i++) {
+		if (bus_space_map(sc->sc_memt, sc->sc_comms[i].sc_addr,
+		    sc->sc_comms[i].sc_size, 0,
+		    &sc->sc_comms[i].sc_memh)) {
+			printf(", can't map memory for community %d\n", i);
+			goto free;
+		}
+
+		sc->sc_comms[i].padbar = bus_space_read_4(sc->sc_memt,
+		    sc->sc_comms[i].sc_memh, SPT_PADBAR);
 	}
 
 	sc->sc_ih = acpi_intr_establish(sc->sc_irq, sc->sc_irq_flags, IPL_BIO,
@@ -167,38 +228,21 @@ sptgpio_attach(struct device *parent, struct device *self, void *aux)
 	sc->sc_node->gpio = &sc->sc_gpio;
 
 	/* Mask all interrupts. */
-	for (i = 0; i < sc->sc_npins; i++) {
-		reg = bus_space_read_4(sc->sc_memt, sc->sc_memh,
-		    sc->sc_pins[i] * 16);
+	for (i = 0; i < sc->sc_ncomms; i++) {
+		struct spt_community c = sc->sc_comms[i];
 
-		/*
-		 * Skip pins configured as direct IRQ.  Those are tied
-		 * directly to the APIC.
-		 */
-		if (reg & SPTGPIO_CONF_DIRECT_IRQ_EN)
-			continue;
-
-		reg &= ~SPTGPIO_CONF_GD_MASK;
-		bus_space_write_4(sc->sc_memt, sc->sc_memh,
-		    sc->sc_pins[i] * 16, reg);
+		for (j = c.first_pin; j <= c.last_pin; j++)
+			sptgpio_irq_mask(sc, j, 1);
 	}
 
 	printf(", %d pins\n", sc->sc_npins);
 
-	/* Register address space. */
-	memset(&arg, 0, sizeof(arg));
-	arg[0].type = AML_OBJTYPE_INTEGER;
-	arg[0].v_integer = ACPI_OPREG_GPIO;
-	arg[1].type = AML_OBJTYPE_INTEGER;
-	arg[1].v_integer = 1;
-	node = aml_searchname(sc->sc_node, "_REG");
-	if (node && aml_evalnode(sc->sc_acpi, node, 2, arg, NULL))
-		printf("%s: _REG failed\n", sc->sc_dev.dv_xname);
-
 	return;
 
 unmap:
-	bus_space_unmap(sc->sc_memt, sc->sc_memh, sc->sc_size);
+	for (i = 0; i < sc->sc_ncomms; i++)
+		bus_space_unmap(sc->sc_memt, sc->sc_comms[i].sc_memh,
+		    sc->sc_comms[i].sc_size);
 free:
 	free(sc->sc_pin_ih, M_DEVBUF, sc->sc_npins * sizeof(*sc->sc_pin_ih));
 }
@@ -210,10 +254,12 @@ sptgpio_parse_resources(int crsidx, union acpi_resource *crs, void *arg)
 	int type = AML_CRSTYPE(crs);
 
 	switch (type) {
-	case LR_MEM32FIXED:
-		sc->sc_addr = crs->lr_m32fixed._bas;
-		sc->sc_size = crs->lr_m32fixed._len;
+	case LR_MEM32FIXED: {
+		sc->sc_ncomms++;
+		sc->sc_comms[sc->sc_ncomms - 1].sc_addr = crs->lr_m32fixed._bas;
+		sc->sc_comms[sc->sc_ncomms - 1].sc_size = crs->lr_m32fixed._len;
 		break;
+	}
 	case LR_EXTIRQ:
 		sc->sc_irq = crs->lr_extirq.irq[0];
 		sc->sc_irq_flags = crs->lr_extirq.flags;
@@ -229,76 +275,24 @@ sptgpio_parse_resources(int crsidx, union acpi_resource *crs, void *arg)
 int
 sptgpio_read_pin(void *cookie, int pin)
 {
-	struct sptgpio_softc *sc = cookie;
-	uint32_t reg;
-
-	reg = bus_space_read_4(sc->sc_memt, sc->sc_memh,
-	    sc->sc_pins[pin] * 16 + 8);
-	return (reg & SPTGPIO_PAD_VAL);
+	return 0;
 }
 
 void
 sptgpio_write_pin(void *cookie, int pin, int value)
 {
-	struct sptgpio_softc *sc = cookie;
-	uint32_t reg;
-
-	reg = bus_space_read_4(sc->sc_memt, sc->sc_memh,
-	    sc->sc_pins[pin] * 16 + 8);
-	if (value)
-		reg |= SPTGPIO_PAD_VAL;
-	else
-		reg &= ~SPTGPIO_PAD_VAL;
-	bus_space_write_4(sc->sc_memt, sc->sc_memh, sc->sc_pins[pin] * 16 + 8,
-	    reg);
 }
 
 void
 sptgpio_intr_establish(void *cookie, int pin, int flags,
     int (*func)(void *), void *arg)
 {
-	struct sptgpio_softc *sc = cookie;
-	uint32_t reg;
-
-	KASSERT(pin >= 0 && pin < sc->sc_npins);
-
-	sc->sc_pin_ih[pin].ih_func = func;
-	sc->sc_pin_ih[pin].ih_arg = arg;
-
-	reg = bus_space_read_4(sc->sc_memt, sc->sc_memh, sc->sc_pins[pin] * 16);
-	reg &= ~SPTGPIO_CONF_GD_MASK;
-	if ((flags & LR_GPIO_MODE) == 0)
-		reg |= SPTGPIO_CONF_GD_LEVEL;
-	if ((flags & LR_GPIO_POLARITY) == LR_GPIO_ACTLO)
-		reg |= SPTGPIO_CONF_GD_TNE;
-	if ((flags & LR_GPIO_POLARITY) == LR_GPIO_ACTHI)
-		reg |= SPTGPIO_CONF_GD_TPE;
-	if ((flags & LR_GPIO_POLARITY) == LR_GPIO_ACTBOTH)
-		reg |= SPTGPIO_CONF_GD_TNE | SPTGPIO_CONF_GD_TPE;
-	bus_space_write_4(sc->sc_memt, sc->sc_memh, sc->sc_pins[pin] * 16, reg);
+	printf("%s\n", __func__);
 }
 
 int
 sptgpio_intr(void *arg)
 {
-	struct sptgpio_softc *sc = arg;
-	uint32_t reg;
-	int rc = 0;
-	int pin;
-
-	for (pin = 0; pin < sc->sc_npins; pin++) {
-		if (pin % 32 == 0) {
-			reg = bus_space_read_4(sc->sc_memt, sc->sc_memh,
-			    SPTGPIO_IRQ_TS_0 + (pin / 8));
-			bus_space_write_4(sc->sc_memt, sc->sc_memh,
-			    SPTGPIO_IRQ_TS_0 + (pin / 8), reg);
-		}
-		if (reg & (1 << (pin % 32))) {
-			if (sc->sc_pin_ih[pin].ih_func)
-				sc->sc_pin_ih[pin].ih_func(sc->sc_pin_ih[pin].ih_arg);
-			rc = 1;
-		}
-	}
-
-	return rc;
+	printf("%s\n", __func__);
+	return 1;
 }
