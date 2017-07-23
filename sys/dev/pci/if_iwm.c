108c6fe1b15af8e1353979a2a04e1caba644a62d
joshua stein <jcs@jcs.org>
2017-07-23 17:55:14 -0500

    iwm: silence annoying warning
    iwm0: unhandled firmware response 0xff/0xb8000010 rx ring 0[16]
    iwm0: unhandled firmware response 0xff/0xb8000010 rx ring 1[166]

--- sys/dev/pci/if_iwm.c
+++ sys/dev/pci/if_iwm.c
@@ -7050,6 +7050,9 @@ iwm_notif_intr(struct iwm_softc *sc)
 			break;
 		}
 
+		case 0x4ff:
+			break;
+
 		default:
 			printf("%s: unhandled firmware response 0x%x/0x%x "
 			    "rx ring %d[%d]\n",
